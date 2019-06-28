3be938385c9d7cb8402cb2a9c6bc3de67d760806
yh <1259535626@qq.com>
2019-06-28 16:56:28 +0800

    day05

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.
+'''
+#config_default.py
+
+config={
+    'db':{
+        'host':'127.0.0.1',
+        'port':3306,
+        'user':'root',
+        'password':'root',
+        'database':'awesome'  
+    },
+    'session':{
+        'secret':'Awesome'
+    }
+}
